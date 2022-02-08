347713105d706fb64f3e6d4c55ebdc9a8762e68f
mohanadft <mohanadfteha@gmail.com>
2022-02-09 02:48:08 +0300

    Finishing About Section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,29 @@
 import React from 'react'
 
 export const About = () => {
+	const progress = [
+		{
+			id: 1,
+			name: 'progress1',
+			percent: '90',
+			title: 'Coding',
+			borderColor: '#fa65b1'
+		},
+		{
+			id: 2,
+			name: 'progress2',
+			percent: '70',
+			title: 'Photoshop',
+			borderColor: '#726ae3'
+		},
+		{
+			id: 3,
+			name: 'progress3',
+			percent: '80',
+			title: 'Animation',
+			borderColor: '#f58b56'
+		}
+	]
 	return (
 		<section className="about">
 			<div className="container">
@@ -11,16 +34,41 @@ export const About = () => {
 					/>
 				</div>
 				<div className="right">
-					<p>about us</p>
-					<h1>Who Is DigiMedia Agency</h1>
+					<h4>about us</h4>
+					<h1>
+						Who Is DigiMedia <span>Agency</span>
+					</h1>
 					<div className="line"></div>
 					<p>
 						We hope this DigiMedia template is useful for your work. You can use
 						this template for any purpose. You may{' '}
-						<a href="#a">contribute a little amount</a>
-						via PayPal to <a href="#a"> support TemplateMo </a>in creating new
-						templates regularly.
+						<a href="#a">contribute a little amount</a> via PayPal to{' '}
+						<a href="#a"> support TemplateMo </a>in creating new templates
+						regularly.
 					</p>
+					<div className="progress">
+						{progress.map(e => {
+							return (
+								<div className={e.name} key={e.id}>
+									<svg>
+										<circle
+											cx="70"
+											cy="70"
+											r="70"
+											style={{
+												strokeDashoffset: `calc(440 - (440 * ${+e.percent}) / 100`,
+												stroke: e.borderColor
+											}}
+										></circle>
+									</svg>
+									<div className="text">
+										<h2>{e.percent}%</h2>
+										<h5>{e.title}</h5>
+									</div>
+								</div>
+							)
+						})}
+					</div>
 				</div>
 			</div>
 		</section>
